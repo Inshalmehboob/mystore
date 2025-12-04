39323df97746136a76541e587fbc9b697effbd42
Inshalmehboob <inshalkshf@gmail.com>
2025-12-04 17:22:13 +0500

    temp rename

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import { BrowserRouter } from "react-router-dom";
-import { ProductProvider } from "./context/ProductContext.jsx";   
-import { CartProvider } from "./context/CartContext.jsx";
+import { ProductProvider } from "./context/productcontext.jsx";   
+import { CartProvider } from "./context/cartcontext.jsx";
 import { OrderProvider } from "./context/ordercontext.jsx";       
 import { AuthProvider } from "./context/authcontext.jsx";   
 ReactDOM.createRoot(document.getElementById("root")).render(
