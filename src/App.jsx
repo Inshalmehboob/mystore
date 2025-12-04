1c806c97bce87d385e73326c1055ff4149d7e552
Inshalmehboob <inshalkshf@gmail.com>
2025-12-05 02:25:32 +0500

    fix: header import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Header from "./components/header.jsx";
+import Header from "./components/Header.jsx";
 import Footer from "./components/footer.jsx";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
